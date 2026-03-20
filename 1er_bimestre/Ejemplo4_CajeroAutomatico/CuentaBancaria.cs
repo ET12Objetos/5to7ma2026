0cdbcdd2400f94a3fb0ddd2fce6d6c74b760fbdd
jonathanvgms <jonathanvgms@gmail.com>
2026-03-20 09:18:36 -0300

    Fix warnigns

--- 1er_bimestre/Ejemplo4_CajeroAutomatico/CuentaBancaria.cs
+++ 1er_bimestre/Ejemplo4_CajeroAutomatico/CuentaBancaria.cs
@@ -1,31 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace Ejemplo4_CajeroAutomatico;
 
-namespace Ejemplo4_CajeroAutomatico
+public class CuentaBancaria
 {
-    // --- OBJETO 1: LA ENTIDAD DE DATOS (CUENTA) ---
-    public class CuentaBancaria
+    private decimal _saldo;
+
+    public CuentaBancaria(decimal saldoInicial)
     {
-        private decimal _saldo;
-
-        public CuentaBancaria(decimal saldoInicial)
-        {
-            _saldo = saldoInicial;
-        }
-
-        // Responsabilidad: Validar y descontar dinero
-        public bool AutorizarRetiro(decimal monto)
-        {
-            if (monto <= _saldo)
-            {
-                _saldo -= monto; // Actualiza su estado interno
-                return true;     // Respuesta al mensaje: ÉXITO
-            }
-            return false;        // Respuesta al mensaje: FALLO
-        }
-
-        public decimal ConsultarSaldo() => _saldo;
+        _saldo = saldoInicial;
     }
+
+    // Responsabilidad: Validar y descontar dinero
+    public bool AutorizarRetiro(decimal monto)
+    {
+        if (monto <= _saldo)
+        {
+            _saldo -= monto; // Actualiza su estado interno
+            return true;     // Respuesta al mensaje: ÉXITO
+        }
+        return false;        // Respuesta al mensaje: FALLO
+    }
+
+    public decimal ConsultarSaldo() => _saldo;
 }
