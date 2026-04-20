33b4d231f69ef1dc4f88f4757366de5a043b413e
jonathanvgms <jonathanvgms@gmail.com>
2026-04-20 11:11:25 -0300

    Ejemplo de conexion a MySQL

---
+++ 1er_bimestre/EjemploMySql/Program.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+
+string connectionString = "Server=localhost;Port=3306;User=root;Password=pass123;Database=ejemplo";
+
+MySqlConnection mySqlConnection = new MySqlConnection(connectionString);
+
+mySqlConnection.Open();
+
+MySqlCommand command = new MySqlCommand("select * from persona;", mySqlConnection);
+
+MySqlDataReader reader = command.ExecuteReader();
+
+while (reader.Read())
+{
+    Console.WriteLine($"{reader["id"]} {reader["nombre"]} {reader["dni"]}");
+}
+
+reader.Close();
+
+mySqlConnection.Close();
