fb536f7ae392e5e8f59b7cb0108e4ea806584f4f
jonathanvgms <jonathanvgms@gmail.com>
2026-07-06 09:20:30 -0300

    Agregao ejemplo dapper

---
+++ 2do_bimestre/EjemploDapper/Persona.cs
@@ -0,0 +1,8 @@
+namespace EjemploDapper;
+
+public class Persona
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = default!;
+    public int Dni { get; set; }
+}
---
+++ 2do_bimestre/EjemploDapper/Program.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using EjemploDapper;
+using MySql.Data.MySqlClient;
+
+List<Persona> personas = new List<Persona>();
+
+string connectionString = "Server=localhost;Port=3306;User=root;Password=pass123;Database=ejemplo";
+
+MySqlConnection mySqlConnection = new MySqlConnection(connectionString);
+
+mySqlConnection.Open();
+
+string query = "select * from persona";
+
+personas = mySqlConnection.Query<Persona>(query).ToList();
+
+foreach (var p in personas)
+{
+    Console.WriteLine($"Id: {p.Id}, Nombre: {p.Nombre}, DNI: {p.Dni}");
+}
+
+mySqlConnection.Close();
